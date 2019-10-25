21d3747859347233aa279e1864c839d3a46192af
Sebastien Cesbron <sebastien@wecasa.fr>
2019-10-25 14:39:50 +0200

    first work on ios module

--- ios/ZohoAsap.m
+++ ios/ZohoAsap.m
@@ -1,21 +1,11 @@
 #import "ZohoAsap.h"
 
-#import <AFNetworking/AFNetworking.h>
-
 @implementation ZohoAsap
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(sampleMethod:(NSString *)stringArgument numberParameter:(nonnull NSNumber *)numberArgument callback:(RCTResponseSenderBlock)callback)
+RCT_EXPORT_METHOD(init:(NSString *)orgId appId(NSString *)appId dataCenterValue(NSString *)dataCenterValue language(NSString *)language)
 {
-    // TODO: Implement some actually useful functionality
-	AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
-	manager.responseSerializer.acceptableContentTypes = [NSSet setWithObject:@"text/plain"];
-	[manager GET:@"https://httpstat.us/200" parameters:nil progress:nil success:^(NSURLSessionTask *task, id responseObject) {
-			callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@ resp: %@", numberArgument, stringArgument, responseObject]]);
-	} failure:^(NSURLSessionTask *operation, NSError *error) {
-			callback(@[[NSString stringWithFormat: @"numberArgument: %@ stringArgument: %@ err: %@", numberArgument, stringArgument, error]]);
-	}];
 }
 
 @end
