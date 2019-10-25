e7bdab48d8fd08a56e4ee149b2ebd416cce0205a
Sébastien Cesbron <sebastien@wecasa.fr>
2019-10-25 16:01:42 +0200

    Start the ios part of the module

--- ios/ZohoAsap.m
+++ ios/ZohoAsap.m
@@ -1,11 +1,27 @@
 #import "ZohoAsap.h"
 
+@import ZohoDeskPortalSDK;
+
 @implementation ZohoAsap
 
 RCT_EXPORT_MODULE()
 
 RCT_EXPORT_METHOD(init:(NSString *)orgId appId(NSString *)appId dataCenterValue(NSString *)dataCenterValue language(NSString *)language)
 {
+    ZDPortalConfiguration * config = [[ZDPortalConfiguration alloc]init];
+    ZDPortalConfiguration.setSDKLanguage = language;
+    config.enableHelpCenter = YES;
+    config.enableCommunity = YES;
+    config.enableCreateTicket = YES;
+    config.enableMyTicket = YES;
+    ZDDataCenter.valueOf(dataCenterValue)
+    [ZohoDeskPortalSDK initializeSDK:orgId appId:appId dataCenter:<#dataCenterValue#> configuration:<#config#>];
+}
+
+RCT_EXPORT_METHOD(startDeskHomeScreen)
+{
+    [ZohoDeskPortalSDK showLiveChatWithController:self];
+    [ZohoDeskPortalSDK showHomePageWithController:self];
 }
 
 @end
